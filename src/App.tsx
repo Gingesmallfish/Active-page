4988e10edaa6070b0c100d1a00dad1be4505eea3
姜小鱼 <1847535232@qq.com>
2024-01-26 19:48:32 +0800

    feat: 完成第一部分和第三步的基础样式

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,16 @@
 import React from 'react';
-import './App.css';
+import './global.scss'
+import FirstSection from "./sections/FirstSection";
+import SecondSection from "./sections/SecondSection";
+import ThirdSection from "./sections/ThirdSection";
 
+import styles from './styles.module.scss'
 function App() {
   return (
-    <div className="App">
-     hello
+    <div className={ styles.app }>
+      <FirstSection />
+      <SecondSection />
+      <ThirdSection />
     </div>
   );
 }
---
+++ src/sections/ThirdSection/index.tsx
@@ -0,0 +1,22 @@
+import {FC} from "react";
+//引入Title
+import Title1Image from "../../assets/title1.jpg"
+import Title2Image from "../../assets/title2.jpg"
+import CommentImage from "../../assets/comment.jpg"
+import styles from "./styles.module.scss"
+const ThirdSection:FC = () => {
+  return(
+      <div className={ styles.thirdSection }>
+
+          <img src={ Title1Image } alt="Title 1"/>
+
+          <img className={ styles.comment } src={ CommentImage } alt="Comment"/>
+
+          <img src={ Title2Image } alt="Title 1"/>
+
+          <img className={ styles.comment } src={ CommentImage } alt="comment"/>
+      </div>
+  )
+}
+
+export default ThirdSection
