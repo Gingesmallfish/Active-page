34d822f95f5ba8184f28278c5d7f0df09b42103e
姜小鱼 <1847535232@qq.com>
2024-01-26 20:52:09 +0800

    feat: 完成 Tabs 高亮

--- src/sections/SecondSection/index.tsx
+++ src/sections/SecondSection/index.tsx
@@ -1,26 +1,55 @@
-import {FC} from "react";
+import {FC, useState} from "react";
 import CartoonImage from '../../assets/cartoon.jpg'
 import MovieImage from '../../assets/movie.png'
 import LifeImage from '../../assets/life.jpg'
 import FoodImage from '../../assets/food.jpg'
 
 import styles from "./styles.module.scss"
+import classNames from "classnames";
 
 const SecondSection:FC = () => {
+    /**
+     * @param tabs[string]
+     */
+    const tabs = [
+        {
+            key: 'cartoon',
+            title: '动画',
+        },
+        {
+            key: 'food',
+            title: '美食',
+        },
+        {
+            key: 'movie',
+            title: '电影',
+        },
+        {
+            key: 'life',
+            title: '生活',
+        }
+    ]
+
+    const  [ activeTab, setActiveTab ] = useState('cartoon')
+
   return(
       <div className={styles.secondSection}>
-          {/* Tabs */}
+          {/*  这里我们 activeTab 单独的抽出来，我们写成数组的形式,然后进行在页面给上渲染  */}
           <ul>
-              <li>动画</li>
-              <li>美食</li>
-              <li>电影</li>
-              <li>生活</li>
+              { tabs.map(tab => (
+                  <li key={ tab.key } onClick={() => setActiveTab(tab.key)}>
+                      <span>{ tab.title }</span>
+                      {/*  下划线line  */}
+                      <span className={ classNames(styles.line, {[ styles.visible ]: activeTab === tab.key}) }/>
+                  </li>
+              ))}
+
           </ul>
           {/*Tab + Content*/}
           <div>
               <section>
                   <h2>动画</h2>
-                  <img src={ CartoonImage } alt="Cartoon"/>
+                  <img src={CartoonImage} alt="Cartoon"/>
               </section>
               <section>
                   <h2>美食</h2>
